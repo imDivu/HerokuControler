480fa97207d656caf174947fa0291c60c3fc4c45
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-22 13:14:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","1275856659:AAEsSfgvNQL4yS7M9p1yu7zqK8nCF_xwkZk")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","4f39e784-aed1-4ddc-a4a1-01ebe05cae03")
     LOGS = os.environ.get("LOGS","")
     SUDO_USERS = [int(user) for user in os.environ.get("SUDO_USERS").split(",")]
     SUPPORT_USERS = [int(user) for user in os.environ.get("SUPPORT_USERS").split(",")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","hinatatest")
